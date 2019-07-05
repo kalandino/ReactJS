bfabfcc12506d598770b4cc325e8cd4b0627cf92
kalandino <kalandino777@gmail.com>
2019-07-05 12:32:14 +0300

    Задание к уроку №8

--- static_src/containers/App.jsx
+++ static_src/containers/App.jsx
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import Layout from './Layout';
 import Profile from './Profile';
 import Message from './Message';
+import InstallPopup from '../components/InstallPopup';
 
 
 export default class App extends React.Component {
@@ -12,11 +13,14 @@ export default class App extends React.Component {
 
   render() {
   	return (
-      <Switch>
-        <Route exact path='/' component={ Layout } />
-        <Route exact path='/profile/' component={ Profile } />
-        <Route exact path='/chat/:chatId/' render={ obj => <Layout chatId={ Number(obj.match.params.chatId) } /> } />
-      </Switch>
+      [
+        <InstallPopup/>,
+        <Switch key='router'>
+          <Route exact path='/' component={ Layout } />
+          <Route exact path='/profile/' component={ Profile } />
+          <Route exact path='/chat/:chatId/' render={ obj => <Layout chatId={ Number(obj.match.params.chatId) } /> } />
+        </Switch>
+      ]
     )
   }
 }
